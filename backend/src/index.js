63d159f6031cc3dd409f6b36a01a044cad367e56
harshkr2926-star <harshkr2926@gmail.com>
2026-08-24 17:45:13 +0530

    Routers and controllers

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,7 @@ import { connectDB } from "./lib/db.js"
 import job from "./lib/cron.js";
 import clerkWebhook from "./webhook/clerk.webhook.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 dotenv.config()
 const app=express()
 
@@ -29,6 +30,7 @@ app.get("/health",(req,res)=>{
 });
 
 app.use("/api/routes",authRoutes)
+app.use("/api/messages",messageRoutes);
 
 
 //if the public directory exists,serve the static files
